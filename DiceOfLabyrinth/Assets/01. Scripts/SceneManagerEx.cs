b31ede78c81992a4e89ab7fe236a3d3517c06225
BAOBAB2231 <136507108+BAOBAB2231@users.noreply.github.com>
2025-06-23 19:47:56 +0900

    ✨ Feat: SceneManagerEx 구현 및 씬 전환 기능 추가
    싱글톤 구현.
    씬 이동, 스택 기반으로 뒤로가기 기능 추가.

---
+++ DiceOfLabyrinth/Assets/01. Scripts/SceneManagerEx.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerEx : MonoBehaviour
+{
+    public static SceneManagerEx Instance;
+    private Stack<string> sceneStack = new Stack<string>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnAdventureClicked()
+    {
+        LoadScene("DungeonSelectScene");
+    }
+
+    public void OnBackClicked()
+    {
+        Debug.Log("뒤로 가기");
+        LoadPreviousScene();
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+        sceneStack.Push(currentScene);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadPreviousScene()
+    {
+        if (sceneStack.Count > 0)
+        {
+            string previousScene = sceneStack.Pop();
+            SceneManager.LoadScene(previousScene);
+        }
+        else
+        {
+            Debug.Log("이전 씬이 없습니다.");
+        }
+
+    }
+}
