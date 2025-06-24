4d75c11e0acf84b134516bd5db50e8ac9abb4d64
BAOBAB2231 <136507108+BAOBAB2231@users.noreply.github.com>
2025-06-24 12:36:51 +0900

    ♻️ Refactor: SceneManagerEx 씬 전환 방식 수정
    로딩 씬을 도입하므로 씬 전환 방식을 수정함. 로딩 씬을 거쳐서 비동기 처리.
    기존 : 현재 씬 → 다음 씬
    변경 : 현재 씬 → 로딩 씬 → 다음 씬

--- DiceOfLabyrinth/Assets/01. Scripts/SceneManagerEx.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/SceneManagerEx.cs
@@ -5,7 +5,9 @@ using UnityEngine.SceneManagement;
 public class SceneManagerEx : MonoBehaviour
 {
     public static SceneManagerEx Instance;
+
     private Stack<string> sceneStack = new Stack<string>();
+    public string nextSceneName { get; private set; } // 로딩 후 이동할 씬 이름
 
     private void Awake()
     {
@@ -33,22 +35,23 @@ public class SceneManagerEx : MonoBehaviour
 
     public void LoadScene(string sceneName)
     {
-        string currentScene = SceneManager.GetActiveScene().name;
-        sceneStack.Push(currentScene);
-        SceneManager.LoadScene(sceneName);
+        string currentScene = SceneManager.GetActiveScene().name; // 현재 씬 이름을 저장
+        sceneStack.Push(currentScene); // 현재 씬을 스택에 추가. 추후에 꺼내면 그게 이전 씬이 됨.
+        nextSceneName = sceneName; // 일단 이동할 씬을 저장해놓고, 먼저 로딩 씬으로 이동
+        SceneManager.LoadScene("LoadingScene");
     }
 
     public void LoadPreviousScene()
     {
-        if (sceneStack.Count > 0)
+        if (sceneStack.Count > 0) // 이전 씬이 있다면,
         {
-            string previousScene = sceneStack.Pop();
-            SceneManager.LoadScene(previousScene);
+            string previousScene = sceneStack.Pop(); // 이전 씬을 스택에서 제거하고 변수에 저장
+            nextSceneName = previousScene; // 일단 이동할 씬을 저장해놓고, 먼저 로딩 씬으로 이동
+            SceneManager.LoadScene("LoadingScene");
         }
-        else
+        else // 이전 씬이 없다면,
         {
             Debug.Log("이전 씬이 없습니다.");
         }
-
     }
 }
