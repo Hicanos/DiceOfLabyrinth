a89c33b0cd2456454c94d20029d967423c887cdc
Hicanos <ruda32168@gmail.com>
2025-06-24 16:23:50 +0900

    ✅ [Add] CharacterSO 자동생성
    Json파일을 Resource에 넣어두고 Tool을 이용해 자동으로 생성하도록 제작했습니다.
    확인된 문제: 한글이 Inspecter 창에 정상적으로 들어가지 않음(한글이름)

--- DiceOfLabyrinth/Assets/01. Scripts/Charcter/SO/CharacterSO.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/Charcter/SO/CharacterSO.cs
@@ -5,56 +5,23 @@ using UnityEngine;
 /// 테스트 캐릭터를 만들기 위한 Scriptable Object
 /// </summary>
 
-enum Grade
-{
-    N,
-    R,
-    SR,
-    SSR,
-    UR    
-}
-
-enum CharacterType
-{
-    Tanker,
-    Dealer,
-    Supporter
-}
-
-enum ElementType
-{
-    Fire,
-    Water,
-    Wind,
-    Light,
-    Dark
-}
-
-[CreateAssetMenu(fileName = "NewCharacter", menuName = "Character/Create New Character")]
+[CreateAssetMenu(fileName = "NewCharacterSO", menuName = "Character/Create CharacterSO")]
 public class CharacterSO : ScriptableObject
 {
-    // 캐릭터 ID(string), 캐릭터 타입, 캐릭터 이름, 캐릭터 최대 레벨, 체력, 공격력, 방어력,
-    // 스폰 시 Object, 정보 표시용 Object(UI), 설명, 스킬(Dictionary. SkillData에서 들고옴),
-    // 시그니쳐 넘버
-    // 크리티컬 확률, 크리티컬 대미지(150%)
-
-    [SerializeField] private string characterID;
-    [SerializeField] private Grade grade;
-    [SerializeField] private CharacterType characterType;
-    [SerializeField] private ElementType elementType;
-    [SerializeField] private string characterName;
-    [SerializeField] private int maxLevel;
-    [SerializeField] private int hp;
-    [SerializeField] private int attack;
-    [SerializeField] private int defense;
-    [SerializeField] private int signatureNumber; // 시그니쳐 넘버
-    [SerializeField] private float criticalChance; // 크리티컬 확률
-    [SerializeField] private float criticalDamage; // 크리티컬 대미지 (150% = 1.5f)
-    [SerializeField] private GameObject spawnObject;
-    [SerializeField] private GameObject infoObject;
-    [SerializeField] private string description;
-
-    // Skill 목록을 Dictionary로 저장해서 Key값(스킬 ID)으로 접근
-    [SerializeField] private Dictionary<string, SkillSO> skills;
-
+    public int key;
+    public string charID;
+    public string nameKr;
+    public string nameEn;
+    public DesignEnums.ClassTypes classType;
+    public int baseATK;
+    public int plusATK;
+    public int baseDEF;
+    public int plusDEF;
+    public int baseHP;
+    public int plusHP;
+    public int critChance;
+    public int critDamage;
+    public DesignEnums.ElementTypes elementType;
+    public int signitureNum;
+    public int diceID;
 }
---
+++ DiceOfLabyrinth/Assets/01. Scripts/Charcter/Scripts/DesignEnums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class DesignEnums
+{
+    public enum ClassTypes
+    {
+        Dealer = 0,
+        Tanker = 1,
+        Suppoter = 2,
+    }
+    public enum ElementTypes
+    {
+        Fire = 0,
+        Glass = 1,
+        Rock = 2,
+        Electro = 3,
+        Water = 4,
+    }
+
+}
