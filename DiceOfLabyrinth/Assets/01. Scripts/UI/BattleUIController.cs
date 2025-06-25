34e30d16a7e8bc5a3b78b7df9f28c353d192e0c8
BAOBAB2231 <136507108+BAOBAB2231@users.noreply.github.com>
2025-06-25 12:32:26 +0900

    ✨Feat: BattleScene UI 전환 기능 및 배경 이미지 추가
    BattleUIController 스크립트를 추가.
    BattleScene UI 기본 레이아웃 제작 (StageSelect ↔ Battle)
    배틀UI의 배경 이미지 추가.
    진행 버튼으로 스테이지 선택과 배틀의 패널 전환 기능 추가.

---
+++ DiceOfLabyrinth/Assets/01. Scripts/UI/BattleUIController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleUIController : MonoBehaviour
+{
+    [SerializeField] private Button backButton;
+    [SerializeField] private Button nextButton;
+
+    [SerializeField] private GameObject stageSelectPanel;
+    [SerializeField] private GameObject battlePanel;
+
+    private void Start()
+    {
+        stageSelectPanel.SetActive(true);
+        battlePanel.SetActive(false);
+
+        backButton.onClick.AddListener(SceneManagerEx.Instance.OnBackClicked);
+        nextButton.onClick.AddListener(OpenBattlePanel);
+    }
+
+    private void OpenBattlePanel()
+    {
+        stageSelectPanel.SetActive(false);
+        battlePanel.SetActive(true);
+    }
+}
