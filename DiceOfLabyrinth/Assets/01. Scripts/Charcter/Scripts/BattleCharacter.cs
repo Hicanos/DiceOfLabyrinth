3a94421469fda589cce185a9f612eb3d50824e8a
Hicanos <ruda32168@gmail.com>
2025-06-25 19:11:06 +0900

    ✨ [Feat] Character.cs
    캐릭터의 배틀/로비 상태의 기본 스크립트 생성

---
+++ DiceOfLabyrinth/Assets/01. Scripts/Charcter/Scripts/BattleCharacter.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 배틀(전투)에서 사용되는 캐릭터 정보 및 기능
+/// (실시간 변동 데이터, 버프 등)
+/// </summary>
+public class BattleCharacter : Character, IDamagable
+{
+    [Header("배틀 상태")]
+    public int CurrentHP;
+    public int CurrentATK;
+    public int CurrentDEF;
+    public int CurrentCritChance;
+    public int CurrentCritDamage;
+    public int FormationBonus;
+    public GameObject GameObject; // 배틀에서 사용되는 게임 오브젝트, 스폰되는 캐릭터 모델
+
+    /// <summary>
+    /// 배틀용 초기화 (로비 정보 기반)
+    /// </summary>
+    public override void Initialize(CharacterSO so, int level = 1)
+    {
+        base.Initialize(so, level);
+        ResetBattleData();
+    }
+
+    /// <summary>
+    /// 배틀 데이터 초기화 (스테이지 시작/종료 시)
+    /// </summary>
+    public void ResetBattleData()
+    {
+        CurrentHP = GetMaxHP();
+        CurrentATK = GetATK();
+        CurrentDEF = GetDEF();
+        CurrentCritChance = characterData.critChance;
+        CurrentCritDamage = characterData.critDamage;
+        FormationBonus = 0;
+        // 기타 실시간 데이터 초기화
+    }
+
+
+    public void ApplyBuff(int atkBonus, int defBonus)
+    {
+        CurrentATK += atkBonus;
+        CurrentDEF += defBonus;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Heal(int amount)
+    {
+        throw new System.NotImplementedException();
+    }
+}
