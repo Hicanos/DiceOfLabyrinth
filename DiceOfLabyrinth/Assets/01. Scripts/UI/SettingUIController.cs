337c8c74c582b3bc0047eb251b0e5b644672349d
BAOBAB2231 <136507108+BAOBAB2231@users.noreply.github.com>
2025-06-26 02:31:09 +0900

    ♻️ Refactor: 로비 씬 UI 전체 구조 정리
    UI 재배치

--- DiceOfLabyrinth/Assets/01. Scripts/UI/SettingUIController.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/UI/SettingUIController.cs
@@ -1,16 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingUIController : MonoBehaviour
 {
+    [SerializeField] private Button settingButton;
+    [SerializeField] private Button settingBg;
+
     [SerializeField] private GameObject settingPanel;
     [SerializeField] private GameObject settingPopup;
 
-    public void OpenSetting()
+    private void Start()
+    {
+        settingButton.onClick.AddListener(OpenSetting);
+        settingBg.onClick.AddListener(CloseSetting);
+    }
+
+    private void OpenSetting()
     {
         settingPanel.SetActive(true);
     }
 
-    public void CloseSetting()
+    private void CloseSetting()
     {
         settingPanel.SetActive(false);
     }
