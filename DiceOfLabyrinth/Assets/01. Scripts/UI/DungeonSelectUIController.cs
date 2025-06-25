d6ba8743b917d8dd693cff8a904e676dba911558
BAOBAB2231 <136507108+BAOBAB2231@users.noreply.github.com>
2025-06-26 02:18:09 +0900

    ✨Feat: 챕터 선택 UI 구현
    DungeonSelectUIController.cs 구현.
    챕터→던전 패널 전환, 뒤로가기 연결.
    DungeonSelectUI 스크립트 이름을 DungeonSelectUIController 로 변경.

---
+++ DiceOfLabyrinth/Assets/01. Scripts/UI/DungeonSelectUIController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonSelectUIController : MonoBehaviour
+{
+    [SerializeField] private Button backButton;
+    [SerializeField] private Button chapter_01_Button;
+    [SerializeField] private Button dungeon_01_Button;
+
+    [SerializeField] private GameObject selectChapterPanel;
+    [SerializeField] private GameObject selectDungeonPanel;
+
+    private void Start()
+    {
+        selectChapterPanel.SetActive(true);
+        selectDungeonPanel.SetActive(false);
+
+        backButton.onClick.AddListener(SceneManagerEx.Instance.OnBackClicked);
+        chapter_01_Button.onClick.AddListener(OpenSelectDungeonPanel);
+        dungeon_01_Button.onClick.AddListener(SceneManagerEx.Instance.OnDungeon1Clicked);
+    }
+
+    private void OpenSelectDungeonPanel()
+    {
+        selectDungeonPanel.SetActive(true);
+    }
+}
