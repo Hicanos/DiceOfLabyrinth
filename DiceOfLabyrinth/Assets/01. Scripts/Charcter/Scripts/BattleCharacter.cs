902966ed21d05a52cfaa8ed5e63d564ac58bf84a
Hicanos <ruda32168@gmail.com>
2025-06-26 17:03:49 +0900

    ✅ [Add] SampleChar

--- DiceOfLabyrinth/Assets/01. Scripts/Charcter/Scripts/BattleCharacter.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/Charcter/Scripts/BattleCharacter.cs
@@ -13,7 +13,13 @@ public class BattleCharacter : Character, IDamagable
     public int CurrentCritChance;
     public int CurrentCritDamage;
     public int FormationBonus;
-    public GameObject GameObject; // 배틀에서 사용되는 게임 오브젝트, 스폰되는 캐릭터 모델
+    public GameObject CharObj; // 배틀에서 사용되는 게임 오브젝트, 스폰되는 캐릭터 모델
+    public GameObject UIChar; // 배틀 UI에서 사용되는 캐릭터 UI 오브젝트
+    // 캐릭터가 사용하는 스킬 데이터를 Header를 이용하여 여기에 등록
+
+    public bool IsDied; // 현재 HP가 0 이하인 경우 사망 상태
+
+
 
     /// <summary>
     /// 배틀용 초기화 (로비 정보 기반)
@@ -39,19 +45,47 @@ public class BattleCharacter : Character, IDamagable
     }
 
 
-    public void ApplyBuff(int atkBonus, int defBonus)
+    public void ApplyATK(int amount)
     {
-        CurrentATK += atkBonus;
-        CurrentDEF += defBonus;
+        CurrentATK += amount;
+    }
+    public void ApplyDEF(int amount)
+    {
+        CurrentDEF += amount;
     }
 
     public void TakeDamage(int damage)
     {
-        throw new System.NotImplementedException();
+        CurrentHP -= damage;
+        if (CurrentHP < 0)
+        {
+            CurrentHP = 0;
+            // 캐릭터 사망 처리
+            IsDied = true;
+        }
     }
 
     public void Heal(int amount)
     {
-        throw new System.NotImplementedException();
+        CurrentHP += amount;
+        if( CurrentHP > GetMaxHP())
+        {
+            CurrentHP = GetMaxHP(); // 최대 HP 초과 방지
+        }
+    }
+
+    public void Attack()
+    {
+        // 공격 로직 구현 - 실제로는 배틀 매니저: Battle에서 플레이어 턴에 호출이 필요해보임. 추가 해야하는 변수가 너무 많음
+        //대미지 계산식: [주사위 눈금 * 족보별계수 + {공격력 - 방어력 * (1-방어력 관통률)}] * (버프 + 아티팩트 + 속성 + 패시브 + 각인)
+        // 예시: int damage = (diceValue * familyCoefficient + (CurrentATK - CurrentDEF * (1 - defensePenetration))) * (buff + artifact + element + passive + engraving);
+        
+        Debug.Log("공격");
+    }
+
+    public int AttackDamage()
+    {
+        //공격 기본 대미지 계산 로직
+        return CurrentATK;
     }
 }
