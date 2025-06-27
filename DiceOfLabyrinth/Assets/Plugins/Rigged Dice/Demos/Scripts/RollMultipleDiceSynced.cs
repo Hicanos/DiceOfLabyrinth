1fcde1eaa6d992bcd26dfe5243929d09292e65d4
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-06-27 16:15:48 +0900

    Chore : chore

--- DiceOfLabyrinth/Assets/Plugins/Rigged Dice/Demos/Scripts/RollMultipleDiceSynced.cs
+++ DiceOfLabyrinth/Assets/Plugins/Rigged Dice/Demos/Scripts/RollMultipleDiceSynced.cs
@@ -24,29 +24,7 @@ namespace PredictedDice.Demo
             {
                 diceAndOutcomeArray[i].outcome = outcome[i];
             }
-        }
-        //public void SetWhiteDiceOutcome(float outcome)
-        //{
-        //    diceAndOutcomeArray[0].outcome = (int)outcome;
-        //}
-
-        //public void SetBlueDiceOutcome(float outcome)
-        //{
-        //    diceAndOutcomeArray[1].outcome = (int)outcome;
-        //}
-
-        //public void SetRedDiceOutcome(float outcome)
-        //{
-        //    diceAndOutcomeArray[2].outcome = (int)outcome;
-        //}
-        //public void SetGreenDiceOutcome(float outcome)
-        //{
-        //    diceAndOutcomeArray[3].outcome = (int)outcome;
-        //}
-        //public void SetPurpleDiceOutcome(float outcome)
-        //{
-        //    diceAndOutcomeArray[4].outcome = (int)outcome;
-        //}
+        }        
 
         public void RollAll()
         {
@@ -57,8 +35,6 @@ namespace PredictedDice.Demo
                 diceAndOutcome.dice.RollDiceWithOutCome(
                     GetRandomForcedRollData(diceAndOutcome.random ? RollData.RandomFace : diceAndOutcome.outcome));
             }
-
-            //StartCoroutine(SLerp());
 
             ProjectionSceneManager.Instance.Simulate();
             foreach (DiceAndOutcome diceAndOutcome in diceAndOutcomeArray)
@@ -80,30 +56,6 @@ namespace PredictedDice.Demo
         private Vector3 GetRandomForce()
         {
             return new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
-        }
-
-        IEnumerator SLerp()
-        {
-            foreach (DiceAndOutcome diceAndOutcome in diceAndOutcomeArray)
-            {
-                if (diceAndOutcome.dice == null) continue;
-                Vector3 vec = diceAndOutcome.dice.gameObject.transform.position;
-                Vector3 vec2 = vec + new Vector3(10, 0, 10);
-                while (currentTime < lerpTime)
-                {
-                    currentTime += Time.deltaTime;
-
-                    if (currentTime >= lerpTime)
-                    {
-                        currentTime = lerpTime;
-                    }
-                    
-                    diceAndOutcome.dice.gameObject.transform.position = Vector3.Slerp(vec, vec2, currentTime / lerpTime);
-                    
-                    yield return null;
-                }
-            }
-            Debug.Log("코루틴 종료");
-        }
+        }        
     }
 }
