3f1b893a64b2d875be7285d1ba2439c31153e21e
OnionKimchi <jungjung21b@gmail.com>
2025-06-30 21:20:56 +0900

    Initial commit
    Set up project structure and add initial files.

---
+++ DiceOfLabyrinth/Assets/01. Scripts/Manager/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
