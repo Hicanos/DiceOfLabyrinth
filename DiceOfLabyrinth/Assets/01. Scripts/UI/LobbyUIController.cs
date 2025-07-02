d5cd224bbdf45c6dcc3fee27f6099e319129717a
BAOBAB2231 <136507108+BAOBAB2231@users.noreply.github.com>
2025-07-02 16:22:56 +0900

    ♻️ Refactor: 안쓰는 코드 삭제, 수정한 던전선택UI 프리팹화

--- DiceOfLabyrinth/Assets/01. Scripts/UI/LobbyUIController.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/UI/LobbyUIController.cs
@@ -1,12 +1,9 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 public class LobbyUIController : MonoBehaviour
 {
-    //[SerializeField] private Button adventureButton;
-
     private void Start()
     {
-        //adventureButton.onClick.AddListener(() => SceneManagerEx.Instance.OnAdventureClicked());
+        //
     }
 }
