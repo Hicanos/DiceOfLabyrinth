614a4b20b08d6e834e09d991f5657400294aef3c
BAOBAB2231 <136507108+BAOBAB2231@users.noreply.github.com>
2025-07-03 17:49:12 +0900

    ✨ Feat: 메세지 팝업들을 통일해서 쓸 스크립트 작성
    수정 필요

--- DiceOfLabyrinth/Assets/01. Scripts/UI/CheckPanel.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/UI/CheckPanel.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 
 public class CheckPanel : MonoBehaviour
 {
-    [SerializeField] private TMP_Text questionText;
+    [SerializeField] private TMP_Text msgText;
+
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+    }
 
     public void Open(string msg)
     {
-        questionText.text = msg;
+        msgText.text = msg;
         gameObject.SetActive(true);
     }
 
