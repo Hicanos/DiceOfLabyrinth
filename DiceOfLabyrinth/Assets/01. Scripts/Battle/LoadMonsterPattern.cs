a56271764932db5e703326e91dd64111fd95c0e0
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-07-10 17:23:33 +0900

    Feat : 에너미 스킬 기능 일부

--- DiceOfLabyrinth/Assets/01. Scripts/Battle/LoadMonsterPattern.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/Battle/LoadMonsterPattern.cs
@@ -1,73 +1,24 @@
-﻿using System.IO;
-using System.Linq;
-using Newtonsoft.Json.Linq;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections.Generic;
 
-public class LoadMonsterPattern
+public class EnemyPatternContainer : MonoBehaviour
 {
-    MonsterPattern pattern = new MonsterPattern();
-    //private const string filePath = "Assets/Resources/Json/tempMonsterPattern.json";
-
-    string index;
-    int patternLength;
-    int patternCount;
-
-    //public void Load()
-    //{
-    //    string jsonString = File.ReadAllText(filePath);
-    //    JObject root = JObject.Parse(jsonString);
-
-    //    pattern.pattern = (JObject)root["Pattern"];
-    //    pattern.skill = (JObject)root["Skill"];
-    //}
+    SOEnemySkill[] enemySkillDatas;
 
     public void PrepareSkill()
     {
-        //if (patternLength == 0)
-        //{
-        //    index = SelectPattern();
-        //}
+        BattleManager battleManager = BattleManager.Instance;
 
-        ////string skillNum = pattern.pattern["Table"][index][patternCount].ToString();
-        ////string skillName = pattern.skill[skillNum]["Name"].ToString();
-        ////string skillDescription = pattern.skill[skillNum]["Description"].ToString();
+        List<int> pattern = battleManager.Enemy.Data.ActiveSkills;
+        int patternLength = battleManager.Enemy.Data.ActiveSkills.Count;
+        int patternIndex = (battleManager.BattleTurn - 1) % patternLength;
 
-        //BattleManager.Instance.Enemy.Data
+        int skill_Index = pattern[patternIndex];
 
-        //BattleManager.Instance.UIValueChanger.ChangeUIText(BattleTextUIEnum.MonsterSkillName, $"{skillName} 준비중");
-        //BattleManager.Instance.UIValueChanger.ChangeUIText(BattleTextUIEnum.MonsterSkillDescription, skillDescription);        
+        SOEnemySkill skill = enemySkillDatas[skill_Index];
+        battleManager.Enemy.currentSkill = skill;
 
-        //patternCount++;
-        //if (patternCount == patternLength)
-        //{
-        //    patternLength = 0;
-        //    patternCount = 0;
-        //}
+        battleManager.UIValueChanger.ChangeUIText(BattleTextUIEnum.MonsterSkillName, $"{skill.name} 준비중");
+        battleManager.UIValueChanger.ChangeUIText(BattleTextUIEnum.MonsterSkillDescription, skill.SkillDescription);
     }    
-
-    private string SelectPattern()
-    {
-        Debug.Log("새 패턴 받아옴");
-        JToken patternIndex = pattern.pattern["Index"];
-
-        //string index = BattleManager.Instance.Enemy.Data.Pattern.ToString();
-
-        patternLength = pattern.pattern["Table"][index].Count();
-
-        return index;
-    }
-}
-
-public class MonsterPattern
-{
-    public JObject pattern;
-    public JObject skill;
-}
-
-public class MonsterSkillData
-{
-    string name;
-    string description;
-
-
 }
