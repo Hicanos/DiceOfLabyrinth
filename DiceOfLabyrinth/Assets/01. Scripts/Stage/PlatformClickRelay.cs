df6f3657fdb6cd444d07dba3c5744363965342c3
OnionKimchi <jungjung21b@gmail.com>
2025-07-17 16:26:29 +0900

    Feat: Add character platform system to battle UI
    Introduced a new PlatformClickRelay script and platform prefab for character placement in battle scenes. Updated BattleUIController to manage platform instantiation, selection, and color highlighting, and to show/hide platforms based on UI state. Adjusted related scene and prefab references to support the new system. Also made minor adjustments to the Gnoll enemy prefab and updated material files.

---
+++ DiceOfLabyrinth/Assets/01. Scripts/Stage/PlatformClickRelay.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class PlatformClickRelay : MonoBehaviour
+{
+    public int platformIndex;
+    public CharacterSO selectedCharacter => StageManager.Instance.stageSaveData.entryCharacters[platformIndex];
+}
