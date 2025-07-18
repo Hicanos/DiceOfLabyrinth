17e4f10f738c2df8f0f5659a5a0521f5ebe78f59
Hicanos <ruda32168@gmail.com>
2025-07-18 11:15:13 +0900

    ✅ [Add] BuffManager
    스킬에서 버프 정보를 가져갈 수 있는 BuffData 생성

--- DiceOfLabyrinth/Assets/01. Scripts/Charcter/SO/ActiveSO.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/Charcter/SO/ActiveSO.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using static DesignEnums;
 
 /// <summary>
 /// 액티브 스킬을 만들기 위한 Scriptable Object
@@ -8,9 +9,9 @@ public class ActiveSO : SkillSO
 {
     public int SkillCost; //소모되는 스킬 코스트
     public string[] BuffIDs; // 버프 아이디 배열
-    public string[] BuffAmounts; // 버프 양 배열
+    public BuffAmount[] BuffAmounts; // 버프 양 배열
     // BuffID[0]은 BuffAmount[0]에 해당하는 버프 양을 적용함
-
+    // BuffID[0]이 Buff_1이고 BuffAmount[0]이 Small이면, Buff_1의 Small 버프가 적용됨
     public int BuffProbability; // 버프 적용 확률(%)
     public int BuffTurn; // 버프가 지속되는 턴
     public float SkillValue; // 스킬 효과 값
