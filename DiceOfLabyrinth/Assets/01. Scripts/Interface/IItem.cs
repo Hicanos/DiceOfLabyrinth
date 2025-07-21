ce2ac31a17b439bff8a80271896ae58ad65428d3
Hicanos <ruda32168@gmail.com>
2025-07-21 18:02:45 +0900

    ✅ [Add] 성장관련 아이템 .cs 생성
    .cs는 생성했는데 구현에 좀 더 고민이 필요합니다. 일단 스크립트만 있다고 생각해주세요

---
+++ DiceOfLabyrinth/Assets/01. Scripts/Interface/IItem.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// 아이템 인터페이스
+/// </summary>
+
+public interface IItem
+{
+    void UseItem(); // 아이템 사용 메서드
+}
---
+++ DiceOfLabyrinth/Assets/01. Scripts/Items/EXPpotion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class EXPpotion : MonoBehaviour, IItem
+{
+    // 아이템 SO 정보 가져오기-해당 아이템의 데이터는 
+
+    public void UseItem()
+    {
+        // EXP 포션은 사용한 대상 캐릭터의 경험치를 증가시킴
+        
+    }
+
+    // EXP 포션은 선택된 캐릭터의 경험치를 증가시킴.
+    // 캐릭터의 아이디를 받고 해당 캐릭터의 경험치 상승 메서드 사용
+
+    public void UseItem(LobbyCharacter targetCharacter, int expAmount)
+    {
+        if (targetCharacter != null)
+        {
+            targetCharacter.AddExp(expAmount);
+            Debug.Log($"EXP 포션 사용: {targetCharacter.CharacterData.nameKr}의 경험치가 {expAmount}만큼 증가했습니다.");
+        }
+        else
+        {
+            Debug.LogWarning("대상 캐릭터가 없습니다.");
+        }
+    }
+
+    //선택된 캐릭터의 데이터를 받는 메서드
+
+
+
+}
