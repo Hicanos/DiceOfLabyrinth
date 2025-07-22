1f07739dbad5ce09b78d498d0dcd835b9af4b182
Hicanos <ruda32168@gmail.com>
2025-07-23 03:48:28 +0900

    ✅ [Add] ItemSO 추가
    ItemSO 추가, 해당 아이템 등록 로직 준비 중

--- DiceOfLabyrinth/Assets/01. Scripts/Items/EXPpotion.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/Items/EXPpotion.cs
@@ -1,22 +1,14 @@
 ﻿using UnityEngine;
 
-public class EXPpotion : MonoBehaviour, IItem
-{    
-    // EXP 포션은 선택된 캐릭터의 경험치를 증가시킴.
-    // 캐릭터의 아이디를 받고 해당 캐릭터의 경험치 상승 메서드 사용
 
-    // 해당 코드는 로비UI에서 선택된 캐릭터에게 호출할 것. 일단 이정도로 OK
-    // 차후 Item Manager에서 보유중인 아이템 개수도 차감할 것.
-    public void UseItem(LobbyCharacter targetCharacter, int expAmount)
-    {
-        if (targetCharacter != null)
-        {
-            targetCharacter.AddExp(expAmount);
-            Debug.Log($"EXP 포션 사용: {targetCharacter.CharacterData.nameKr}의 경험치가 {expAmount}만큼 증가했습니다.");
-        }
-        else
-        {
-            Debug.LogWarning("대상 캐릭터가 없습니다.");
-        }
-    }
+/// <summary>
+/// 경험치 포션 SO 클래스 
+/// </summary>
+
+[CreateAssetMenu(fileName = "NewEXPpotion", menuName = "Items/EXPpotion")]
+public class EXPpotion : ItemSO
+{
+    //ItemSO에서 정의된 기본적인 속성을 제외한 고유 속성 정의
+    [Header("경험치 포션 속성")]
+    public int ExpAmount; // 포션 사용 시 획득하는 경험치 양
 }
