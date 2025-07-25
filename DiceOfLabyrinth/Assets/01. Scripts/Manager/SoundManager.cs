661b78f133763af5c72e91e3af32e3fee50b8680
OnionKimchi <jungjung21b@gmail.com>
2025-07-25 19:59:09 +0900

    Add SoundManager and integrate lobby audio
    Introduced SoundManager for centralized BGM and SFX control, added audio sources and sound assets to the project, and updated LobbyUIController to play lobby background music on enable. Scene and UI layout adjustments were made to support the new audio system.

---
+++ DiceOfLabyrinth/Assets/01. Scripts/Manager/SoundManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using static SoundManager;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource bgmSource;// 배경음악 재생용
+    [SerializeField] private AudioSource sfxSource;// SFX 재생용
+
+    [Header("Volume Settings")]
+    [Range(0f, 1f)] public float bgmVolume = 1f;
+    [Range(0f, 1f)] public float sfxVolume = 1f;
+
+    [SerializeField] private List<SoundEntry> soundEntries;
+    public List<SoundEntry> SoundEntries => soundEntries;
+
+    private Dictionary<SoundType, AudioClip> soundDict;
+    public enum SoundType { ButtonClick, // UI 관련 사운드
+        BGM_Lobby,
+        BGM_Battle,
+        BGM_Victory,
+        BGM_GameOver,
+        BGM_Title,
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        { 
+            Instance = this;
+        }
+    }
+    public void PlayBGM(SoundType type) // 배경음악 재생
+    {
+        if (soundDict.TryGetValue(type, out var clip))
+        {
+            // 같은 클립이면 재생 생략
+            if (bgmSource.clip == clip && bgmSource.isPlaying)
+                return;
+            bgmSource.clip = clip;
+            bgmSource.volume = bgmVolume;
+            bgmSource.Play();
+        }
+        else
+        {
+            bgmSource.Stop(); // 해당 타입의 클립이 없으면 현재 재생 중인 BGM을 중지
+        }
+    }
+    public void StopBGM()
+    {
+        bgmSource.Stop();
+    }
+
+    public void PlaySFX(SoundType type) // SFX 재생
+    {
+        if (soundDict.TryGetValue(type, out var clip))
+        {
+            sfxSource.PlayOneShot(clip);
+        }
+    }
+    public void SetBGMVolume(float volume) // 배경음악 볼륨 설정
+    {
+        bgmVolume = Mathf.Clamp01(volume);
+        bgmSource.volume = bgmVolume;
+    }
+
+    public void SetSFXVolume(float volume) // SFX 볼륨 설정
+    {
+        sfxVolume = Mathf.Clamp01(volume);
+    }
+}
+
+[System.Serializable]
+public class SoundEntry
+{
+    [SerializeField] private string soundName;
+    public SoundType type;
+    public AudioClip clip;
+}
