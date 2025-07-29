00d6046068d5cbbbd4b89f0707c746056e946d2b
OnionKimchi <jungjung21b@gmail.com>
2025-07-29 10:58:27 +0900

    Add character summon UI and update controllers
    Enabled character summon panels and popups in the lobby scene. Updated CharacterUIController to manage unowned character lists and popups. Added UI references and summon button logic to LobbyUIController for switching between lobby and summon panels.

--- DiceOfLabyrinth/Assets/01. Scripts/UI/CharacterUIController.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/UI/CharacterUIController.cs
@@ -1,12 +1,46 @@
-﻿using UnityEngine;
+﻿using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 public class CharacterUIController : MonoBehaviour
 {
     [Header("Panels")]
     [SerializeField] private GameObject characterPanel;
+    [Header("Popups")]
+    [SerializeField] private GameObject characterListPopup;
+    [SerializeField] private GameObject characterInfoPopup;
+
+    [Header("CharacterList")]
+    [SerializeField] private List<CharacterSO> unownedCharacters = new List<CharacterSO>(); // 소유하지 않은 캐릭터 목록
 
     private void OnEnable()
     {
         characterPanel.SetActive(true);
+        characterListPopup.SetActive(true);
+        characterInfoPopup.SetActive(false);
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+        UnownedCharactersRefresh();
+    }
+    // 소유하지 않은 캐릭터 목록을 갱신하는 메서드
+    private void UnownedCharactersRefresh()
+    {
+        unownedCharacters.Clear();
+
+        var ownedCharIDs = new HashSet<string>(
+            CharacterManager.Instance.OwnedCharacters.Select(oc => oc.CharacterData.charID)
+        );
+
+        foreach (var pair in CharacterManager.Instance.AllCharacters)
+        {
+            if (!ownedCharIDs.Contains(pair.Key))
+            {
+                unownedCharacters.Add(pair.Value);
+            }
+        }
     }
 }
--- DiceOfLabyrinth/Assets/01. Scripts/UI/LobbyUIController.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/UI/LobbyUIController.cs
@@ -2,8 +2,17 @@
 
 public class LobbyUIController : MonoBehaviour
 {
+    [Header("UI Elements")]
+    [SerializeField] private GameObject lobbyPanel;
+    [SerializeField] private GameObject summonCharactersPanel;
     private void OnEnable()
     {
         SoundManager.Instance.PlayBGM(SoundManager.SoundType.BGM_Lobby);
+    }
+
+    public void OnClickSummonButton()
+    {
+        lobbyPanel.SetActive(false);
+        summonCharactersPanel.SetActive(true);
     }
 }
