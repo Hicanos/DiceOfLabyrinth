2fd9ee6f1198de6d93446fad1a2090359bb7e49c
Hicanos <ruda32168@gmail.com>
2025-07-31 11:52:54 +0900

    ✨ [Feat] 튜토리얼 완료 여부 저장
    튜토리얼 완료 여부 저장

--- DiceOfLabyrinth/Assets/01. Scripts/Manager/DataSaver.cs
+++ DiceOfLabyrinth/Assets/01. Scripts/Manager/DataSaver.cs
@@ -21,7 +21,9 @@ public class DataSaver
     [Serializable]
     public class UserData
     {
-        //유저의 레벨, 재화 정보 보관
+        // 튜토리얼 클리어 여부
+        public bool isLobbyTutorialCompleted = false;
+        // TutorialManager의 값을 저장/로드
     }
 
     [Serializable]
@@ -371,6 +373,12 @@ public class DataSaver
             SyncCharacterData();
             SyncItemData();
 
+            // TutorialManager 튜토리얼 완료 여부 동기화
+            if (TutorialManager.Instance != null)
+            {
+                SaveData.userData.isLobbyTutorialCompleted = TutorialManager.Instance.isLobbyTutorialCompleted;
+            }
+
             // StageSaveData → StageData 변환 및 저장
             if (StageManager.Instance != null && StageManager.Instance.stageSaveData != null)
                 SaveData.stageData = new StageData(StageManager.Instance.stageSaveData);
@@ -455,6 +463,11 @@ public class DataSaver
                 Debug.Log($"chapterStates null? {SaveData.stageData.chapterStates == null}");
                 // SO 복원은 GameManager에서 Addressables 로드 완료 후 호출
 
+                // 튜토리얼 완료 여부를 TutorialManager에 반영
+                if (TutorialManager.Instance != null)
+                {
+                    TutorialManager.Instance.isLobbyTutorialCompleted = SaveData.userData.isLobbyTutorialCompleted;
+                }
             }
             else
             {
