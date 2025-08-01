fdb78673154b698250a400a1c5818a9f96bc00e6
BAOBAB2231 <136507108+BAOBAB2231@users.noreply.github.com>
2025-08-01 14:00:38 +0900

    ✨ Feat: 설정, 일시정지에 포함된 모든 UI 버튼 연결

---
+++ DiceOfLabyrinth/Assets/01. Scripts/UI/SettingPopup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingPopup : MonoBehaviour
+{
+    [SerializeField] private Button closeBg;
+
+    public void OnClickCloseButton()
+    {
+        gameObject.SetActive(false);
+    }
+}
