e675b54a8b4c5472c2e1a02630288395471b3d27
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-08-07 16:24:49 +0900

    Chore : 에너미 패시브 개발위한 기초 코드, SO

---
+++ DiceOfLabyrinth/Assets/01. Scripts/Battle/EnemySkills/Passive/SOEnemyPassive.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+
+public enum EnemyPassiveEffectEnum
+{
+    AdditionalAtk,
+    AdditionalDef,
+    AttackTargetBack,
+    AttackTargetHighestAtk,
+    RestoreHP,
+    StrongWill,
+    GetBarrier,
+    LifeSteal
+}
+
+public enum EnemyPassiveConditionEnum
+{
+    HPRatio,
+    StartBattle,
+    UseSkillIndex
+}
+
+[CreateAssetMenu(fileName = "EnemySkills", menuName = "EnemySkill/Passive")]
+public class SOEnemyPassive : ScriptableObject
+{
+    public int Index;
+    public string Name;
+    public string Description;
+    public int UseCount;
+    public EnemyPassiveEffect[] Effects;
+}
+
+[Serializable]
+public class EnemyPassiveEffect
+{
+    public EnemyPassiveEffectEnum EffectType;
+    public int EffectValue;
+
+    public EnemyPassiveConditionEnum ConditionType;
+    public int ConditionValue;    
+}
